3000ead6206bfd122b5197a54a6b560ce9aaa7d6
James Donald <jdonald.x@gmail.com>
2017-11-20 03:26:42 +0000

    Fix z-culling

--- source/Irrlicht/CEGLManager.cpp
+++ source/Irrlicht/CEGLManager.cpp
@@ -79,6 +79,7 @@ static void init_ogl(CUBE_STATE_T *state)
       EGL_GREEN_SIZE, 8,
       EGL_BLUE_SIZE, 8,
       EGL_ALPHA_SIZE, 8,
+      EGL_DEPTH_SIZE, 16,
       EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
       EGL_NONE
    };
